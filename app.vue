2113edc1b924e5abdd5cb86470e1056ca34f16c8
Nikita Dmitriev <dmitrevnik@gmail.com>
2025-04-25 23:31:53 +0300

    Add scss, prettier, eslint, tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-   <div>
-      <h1>Hello world</h1>
-   </div>
+   <h1 class="text-[20rem]">hello world</h1>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-   modules: ['@nuxt/eslint'],
-   devtools: { enabled: true },
-   compatibilityDate: '2024-11-01',
-   eslint: {
-      config: {
-         stylistic: {
-            indent: 3,
-            quotes: 'single',
-            semi: true,
-         },
-      },
+   modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+   devtools: {
+      enabled: true,
    },
-})
+   css: ["@/assets/css/main.scss"],
+   compatibilityDate: "2024-11-01",
+   tailwindcss: {},
+});
