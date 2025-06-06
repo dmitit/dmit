4d466cf851817bd2d697b6d04f0f13d80fdeb10b
dmitrevnik <dmitrevnik@gmail.com>
2025-06-06 20:53:39 +0300

    Add localeSwitcher

--- components/PrimaryLocaleSwitcher.vue
+++ components/PrimaryLocaleSwitcher.vue
@@ -1,7 +1,63 @@
+<script setup>
+const { locale, locales } = useI18n();
+
+const isOpen = ref(true);
+
+const switchLocalePath = useSwitchLocalePath();
+
+const availableLocales = computed(() => {
+   return locales.value.filter((i) => i.code !== locale.value);
+});
+</script>
+
 <template>
-   <button
-      class="flex h-[20px] w-[20px] items-end justify-center bg-primary text-xs text-white"
-   >
-      <span class="font-medium">en</span>
-   </button>
+   <div class="h-[20px] w-[20px]">
+      <div
+         class="relative h-full w-full"
+         @mouseenter="isOpen = true"
+         @mouseleave="isOpen = false"
+      >
+         <button
+            class="ring-primary-900 flex h-full w-full bg-primary text-xs font-medium text-background ring-1"
+         >
+            <span class="pl-[1px]">
+               {{ locale }}
+            </span>
+         </button>
+         <Transition
+            name="dropdown"
+            enter-active-class="transition-all duration-100"
+            leave-active-class="transition-all duration-300"
+            enter-from-class="opacity-0 scale-y-0"
+            enter-to-class="opacity-100 scale-y-100"
+            leave-from-class="opacity-100 scale-y-100"
+            leave-to-class="opacity-0 scale-y-0"
+         >
+            <div
+               v-if="isOpen"
+               class="ring-primary-900 absolute left-0 z-10 w-full origin-top ring-1"
+            >
+               <ul>
+                  <li
+                     v-for="loc in availableLocales"
+                     :key="loc.code"
+                     class="h-[20px] w-full cursor-pointer bg-background text-xs font-medium text-primary hover:shadow-[inset_0_0_2px_var(--color-primary-700)]"
+                     @click="
+                        () => {
+                           switchLocalePath(loc.code);
+                           isOpen = false;
+                        }
+                     "
+                  >
+                     <NuxtLink :to="switchLocalePath(loc.code)" class="flex">
+                        <span class="pl-[1px]">
+                           {{ loc.code }}
+                        </span>
+                     </NuxtLink>
+                  </li>
+               </ul>
+            </div>
+         </Transition>
+      </div>
+   </div>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,25 @@ export default <Partial<Config>>{
       extend: {
          colors: {
             background: "var(--color-bg)",
+            "background-100": "var(--color-bg-100)",
+            "background-300": "var(--color-bg-300)",
+            "background-500": "var(--color-bg-500)",
+            "background-700": "var(--color-bg-700)",
+            "background-900": "var(--color-bg-900)",
+
             primary: "var(--color-primary)",
+            "primary-100": "var(--color-primary-100)",
+            "primary-300": "var(--color-primary-300)",
+            "primary-500": "var(--color-primary-500)",
+            "primary-700": "var(--color-primary-700)",
+            "primary-900": "var(--color-primary-900)",
+
             accent: "var(--color-accent)",
+            "accent-100": "var(--color-accent-100)",
+            "accent-300": "var(--color-accent-300)",
+            "accent-500": "var(--color-accent-500)",
+            "accent-700": "var(--color-accent-700)",
+            "accent-900": "var(--color-accent-900)",
          },
          fontSize: {
             xs: "0.9rem",
